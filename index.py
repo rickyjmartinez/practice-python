912370cd9d11d077ea3334cab6134eac1d3802a5
ricky martinez <rickyjmartinez2@gmail.com>
2023-10-27 15:29:51 -0400

    python variable practice

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
-print('Welcome to Python 101!2')
-print('Welcome to Python 101')
- 
-print('create hammer')
-print('create nail')
-print('use hammer and nails')
+failed_subjects="2" 
+name='John'
+
+print('Dear Mrs Badger')
+print('Your son ' + name + ' is failing ' + failed_subjects +  ' subjects.')
+print(name + ' will need to redo ' + failed_subjects + ' courses.')
+name="Eric"
+print(name +' is doing well in geography.') 
