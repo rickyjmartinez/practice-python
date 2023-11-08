206b877a39a7badb6a02afc1def48e192e1e07b5
ricky martinez <rickyjmartinez2@gmail.com>
2023-11-08 14:41:22 -0500

    python practice

--- index.py
+++ index.py
@@ -1,8 +1,17 @@
-failed_subjects="2" 
-name='John'
+msg='Welcome to Python 101: Strings'
+print(msg)
+# print(msg.upper())
+# print(msg.lower())
+# print(msg.capitalize())
+# print(msg.title())
 
-print('Dear Mrs Badger')
-print('Your son ' + name + ' is failing ' + failed_subjects +  ' subjects.')
-print(name + ' will need to redo ' + failed_subjects + ' courses.')
-name="Eric"
-print(name +' is doing well in geography.') 
+# print(len(msg))
+# print(msg.count('o'))
+
+# print(msg[-1]) #negatives give last letters
+# print(msg[:7]) #this gives letters from the first index to the second index
+
+msg='welcome to Python 101: Strings'
+msg1=msg[18]+' '+msg[:8]+msg[25:29]+msg[7:11]+msg[13]+msg[12]+msg[2]+msg[1]+msg[-5]  
+print(msg1.title())
+print(msg1[::-1].title())
